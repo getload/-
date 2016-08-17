e668ef27a0f09b8cb96a927eb096cbdf2fae867d
mac <mac@macdemacbook-air.local>
2016-08-17 19:20:21 +0800

    Initial Commit

---
+++ 时间倒计时/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  时间倒计时
+//
+//  Created by mac on 16/8/17.
+//  Copyright © 2016年 huihai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
